f0db3d5490071f5aa406ff54c773dc94d8c8f1a9
Rahat Adnan <sthep90@gmail.com>
2025-09-09 23:42:01 +0600

    fixed major issue in GlslX.cpp::in building program

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,26 @@
 #include<iostream>
-#include "./GLX/Glx.h"
-int main() {
-    const std::string vs = R"(D:/Projects/Personal/CG/TriGL/src/shaders/vertex.glsl)";
-    const std::string fs = R"(D:/Projects/Personal/CG/TriGL/src/shaders/fragment.glsl)";
-    GLX glx;
-    glx.ShaderTool().setFragmentShaderPath(fs);
-    glx.ShaderTool().setVertexShaderPath(vs);
+#include <memory>
 
-    glx.ShaderTool().logFragmentShader();
-    glx.ShaderTool().logVertexShader();
+#include "./GLX/Glx.h"
+const std::string vs = R"(D:/Projects/Personal/CG/TriGL/src/shaders/vertex.glsl)";
+const std::string fs = R"(D:/Projects/Personal/CG/TriGL/src/shaders/fragment.glsl)";
+unsigned int program;
+int main() {
+    GLX glx;;
+
+
+    glx.addPostLaunchProcedure([&]() {
+        glx.ShaderTool().setFragmentShaderPath(fs);
+        glx.ShaderTool().setVertexShaderPath(vs);
+        glx.ShaderTool().buildProgram();
+    });
+
+
+
+
     glx.onTick([]() {
         glClear(GL_COLOR_BUFFER_BIT);
+        //glUseProgram( glx.ShaderTool().getProgram());
        glClearColor(0.3, 1., 0.7, 1.0);
     });
 
