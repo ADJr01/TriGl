25b5e211f23c90d2960c4911b638b9351a3c379a
Rahat Adnan <sthep90@gmail.com>
2025-09-10 01:03:08 +0600

    Included some common predefined vars in GLX

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,8 +1,7 @@
 #version 330 core
-
 layout (location = 0) in vec3 vertPos;
 layout (location = 1) in vec3 vertColor;
-out ve3 fragColor;
+out vec3 fragColor;
 
 void main(){
     gl_Position = vec4(vertPos,1.0);
