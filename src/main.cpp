08d7ce98654cbc7191ee84c33eee90b97a5a660c
Rahat Adnan <sthep90@gmail.com>
2025-09-11 00:51:37 +0600

    Fixed Few Issues related to Shader Compilation

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,32 @@
 const std::string vs = R"(D:/Projects/Personal/CG/TriGL/src/shaders/vertex.glsl)";
 const std::string fs = R"(D:/Projects/Personal/CG/TriGL/src/shaders/fragment.glsl)";
 unsigned int program;
+uint VAO, VBO;
 int main() {
     GLX glx;;
-    glx.buildMode(BUILD_MODE::PROD);
+    glx.buildMode(BUILD_MODE::DEV);
+    //creating vertices
+    float vertecies[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+    glx.ShaderTool().setFragmentShaderPath(fs);
+    glx.ShaderTool().setVertexShaderPath(vs);
+
 
     glx.addPostLaunchProcedure([&]() {
-        glx.ShaderTool().setFragmentShaderPath(fs);
-        glx.ShaderTool().setVertexShaderPath(vs);
+        glGenVertexArrays(1,&VAO);
+        glBindVertexArray(VAO);
+        glGenBuffers(1,&VBO);
+        glBindBuffer(GL_ARRAY_BUFFER,VBO);
+        glBufferData(GL_ARRAY_BUFFER,sizeof(vertecies),vertecies,GL_STATIC_DRAW);
+        glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,nullptr);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER,0);
+        glBindVertexArray(0);
         glx.ShaderTool().buildProgram();
+
     });
 
 
@@ -21,7 +39,11 @@ int main() {
     glx.onTick([&]() {
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram( glx.ShaderTool().getProgram());
-       glClearColor(0.3, 1., 0.7, 1.0);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES,0,3);
+       glClearColor(0.0, 0.0, 0.0, 1.0);
+        glBindVertexArray(0);
+        glUseProgram(0);
     });
 
     glx.launch();
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,9 +1,8 @@
 #version 330 core
 layout (location = 0) in vec3 vertPos;
-layout (location = 1) in vec3 vertColor;
 out vec3 fragColor;
 
 void main(){
     gl_Position = vec4(vertPos,1.0);
-    fragColor = vertColor;
+    fragColor = vec3(0.25,0.77,0.98);
 }
