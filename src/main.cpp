f4bb241bd032a59439ba0629e0d8cb25b66abc0d
Rahat Adnan <sthep90@gmail.com>
2025-09-11 15:01:47 +0600

    Playing with OpenGL

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,20 @@
 const std::string vs = R"(D:/Projects/Personal/CG/TriGL/src/shaders/vertex.glsl)";
 const std::string fs = R"(D:/Projects/Personal/CG/TriGL/src/shaders/fragment.glsl)";
 unsigned int program;
-uint VAO, VBO;
+uint VAO;
+std::array<uint,2> VBOs;
 int main() {
     GLX glx;;
     glx.buildMode(BUILD_MODE::DEV);
     //creating vertices
-    float vertecies[] = {
-             //? Vertex         //?COLOR
-        -0.5f, -0.5f, 0.0f,  0.0f,1.0f,0.0f,
-        0.5f, -0.5f, 0.0f,  0.0f,0.0f,1.0f,
-        0.0f, 0.5f, 0.0f,  1.0f,0.0f,0.0f,
+    std::array<float,9> positions = {
+             //? POS         //?COLOR
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f,
+    };
+    std::array<int,3> index_list = {
+        1,0,2,
     };
     glx.ShaderTool().setFragmentShaderPath(fs);
     glx.ShaderTool().setVertexShaderPath(vs);
@@ -23,13 +27,20 @@ int main() {
     glx.addPostLaunchProcedure([&]() {
         glGenVertexArrays(1,&VAO);
         glBindVertexArray(VAO);
-        glGenBuffers(1,&VBO);
-        glBindBuffer(GL_ARRAY_BUFFER,VBO);
-        glBufferData(GL_ARRAY_BUFFER,sizeof(vertecies),vertecies,GL_STATIC_DRAW);
-        glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,nullptr);
+        glGenBuffers(2,VBOs.data());
+        //?position
+        glBindBuffer(GL_ARRAY_BUFFER,VBOs[0]);
+        glBufferData(GL_ARRAY_BUFFER,positions.size()*sizeof(float),positions.data(),GL_STATIC_DRAW);
+        glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
         glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER,0);
-        glBindVertexArray(0);
+
+        //?Color
+        glBindBuffer(GL_ARRAY_BUFFER,VBOs[1]);
+        glBufferData(GL_ARRAY_BUFFER,index_list.size()*sizeof(float),index_list.data(),GL_STATIC_DRAW);
+        glVertexAttribIPointer(1,1,GL_INT,sizeof(float),(void*)0);
+        glEnableVertexAttribArray(1);
+
+
         glx.ShaderTool().buildProgram();
 
     });
@@ -40,7 +51,7 @@ int main() {
         glUseProgram( glx.ShaderTool().getProgram());
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES,0,3);
-       glClearColor(0.0, 0.0, 0.0, 1.0);
+        glClearColor(0.0, 0.0, 0.0, 1.0);
         glBindVertexArray(0);
         glUseProgram(0);
     });
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,8 +1,16 @@
 #version 330 core
 layout (location = 0) in vec3 vertPos;
+layout (location = 1) in int vertIndex;
 out vec3 fragColor;
+
+
+const vec3 Color[3] = vec3[3](
+        vec3(1.0, 0.0, 0.0),
+        vec3(0.0, 0.0, 1.0),
+        vec3(1.0, 1.0, 0.0)
+);
 
 void main(){
     gl_Position = vec4(vertPos,1.0);
-    fragColor = vec3(0.25,0.77,0.98);
+    fragColor = Color[vertIndex];
 }
