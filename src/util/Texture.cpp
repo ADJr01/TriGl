e944f5f642761af0a9c1849996cdc5c1488dcb26
Rahat Adnan <sthep90@gmail.com>
2025-09-22 02:20:54 +0600

    working on texture loading

--- src/util/Texture.cpp
+++ src/util/Texture.cpp
@@ -8,7 +8,12 @@
 
 Texture::Texture(const char* pathToTexture) {
     int width,height,channels;
-    glx_type::uchar* texture_data = stbi_load(pathToTexture,&width,&height,&channels,STBI_rgb_alpha);
+    glx_type::uchar* texture_pixels = stbi_load(pathToTexture,&width,&height,&channels,STBI_rgb_alpha);
+    //create texture
+    glCreateTextures(GL_TEXTURE_2D,1,&this->textureID);
+
+    //upload to GPU
+    glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,width,height,0,GL_RGBA,GL_UNSIGNED_BYTE,texture_pixels);
 }
 
 Texture::~Texture() {
