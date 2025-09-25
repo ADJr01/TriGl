765a5d9b4afb813b2848fbbe8da90737fd38bc0f
Rahat Adnan <sthep90@gmail.com>
2025-09-25 23:57:31 +0600

    experimenting with textures

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -10,8 +10,8 @@ uniform sampler2D materialMask;
 
 void main(){
     vec3 baseColor = texture(material,textureCoord).rgb;
-    float dx = sqrt(pow(PI,time)*pow(textureCoord.x,2.0)*pow(textureCoord.y,2.0));
+    float dx = sqrt(PI*pow(textureCoord.x-positionData.x,2.0)*pow(textureCoord.y-positionData.y,2.0));
     float alpha = sin(dx * (sin((texture(materialMask,textureCoord).r * textureCoord.x)) * cos(texture(materialMask,textureCoord).g * textureCoord.y)));
-    pixelColor =vec4(baseColor,alpha);
+    pixelColor =alpha*vec4(baseColor,1.0);
 }
 
