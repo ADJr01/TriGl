68c4874d215d91b82d65f86f641f8e1fc6c91868
Rahat Adnan <sthep90@gmail.com>
2025-09-29 02:02:04 +0600

    experimenting with shader

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -10,7 +10,7 @@ uniform sampler2D materialMask;
 void main(){
     vec3 baseTexture = texture(material,textureCoord).rgb;
     float mask = texture(materialMask,textureCoord).r;
-    float dx =   length(vec2(cos(PI*length(textureCoord)),sin(PI*length(textureCoord))));
+    float dx =   sin(PI * pow(length(textureCoord),2.0));
     float effect = smoothstep(mask + 0.1,0.9,dx) - smoothstep(0.9,mask - 0.1,dx);
     fragColor =  vec4(effect * baseTexture,1.0);
 }
