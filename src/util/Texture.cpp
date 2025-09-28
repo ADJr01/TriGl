9cdb2b211508a542b0f546cdbb5b76e87a4e4551
Rahat Adnan <sthep90@gmail.com>
2025-09-29 01:48:06 +0600

    experimenting with shader

--- src/util/Texture.cpp
+++ src/util/Texture.cpp
@@ -30,7 +30,7 @@ Texture::Texture(const char* pathToTexture) {
     //configuring sampler
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
     //? define borderColor as we have CLAMP the texture to fit with border
     glTexParameterfv(GL_TEXTURE_2D,GL_TEXTURE_BORDER_COLOR,borderColor);
