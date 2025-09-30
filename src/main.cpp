bfc316126a837ae0acd5f5a2dc2f6a92bf7513f2
Rahat Adnan <sthep90@gmail.com>
2025-09-30 12:21:18 +0600

    experimenting with shader

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,15 @@
 #include "util/Texture.h"
 const std::string vs = R"(D:/Projects/Personal/CG/TriGL/src/shaders/vertex.glsl)";
 const std::string fs = R"(D:/Projects/Personal/CG/TriGL/src/shaders/fragment.glsl)";
-const std::string texturePath = R"(D:/Projects/Personal/CG/TriGL/src/asset/brick.jpg)";
-const std::string maskTexturePath = R"(D:/Projects/Personal/CG/TriGL/src/asset/brick_mask.png)";
+const std::string texturePath = R"(D:/Projects/Personal/CG/TriGL/src/asset/jerry.png)";
+const std::string maskTexturePath = R"(D:/Projects/Personal/CG/TriGL/src/asset/jerry_mask.png)";
 unsigned int program;
 int uniformModel,iTime;
 Texture* texture = nullptr;
 Texture* maskTexture = nullptr;
 constexpr glx_type::uint VERTEX_TO_DRAW_COUNT = 6;
 constexpr float toRadians = std::numbers::pi/180;
-constexpr float CUBE_SIZE = 0.3;
+constexpr float CUBE_SIZE = 0.8;
 int main() {
     GLX glx;
     glx.setVersionMajor(4);
