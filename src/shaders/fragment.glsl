16b10587014ebe479c411bd4a9d193f55b269bb9
Rahat Adnan <sthep90@gmail.com>
2025-10-06 19:00:53 +0600

    shader practice

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -15,7 +15,7 @@ float axisSmooth(float axis,float len){
 }
 void main(){
     vec3 baseTexture = texture(material,textureCoord).rgb;
-    float mask = texture(materialMask,textureCoord).r;
+    float mask = texture(materialMask,textureCoord).g;
     float tX = baseTexture.r*sin(time)*log(PI);
     float tY = baseTexture.g* cos(time)*log(PI);
     float len = axisSmooth(tX,length(vec2(tX,tY)));
