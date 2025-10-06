ac20b4e5e4389b459ac5f3d0af14085da8386d84
Rahat Adnan <sthep90@gmail.com>
2025-10-06 19:09:50 +0600

    shader practice

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -9,16 +9,16 @@ uniform sampler2D materialMask;
 
 float axisSmooth(float axis,float len){
     axis = pow(axis,2.0);
-    float a = smoothstep(0.09,axis-0.1,len);
-    float b = smoothstep(axis-0.1,0.09,len);
+    float a = smoothstep(0.1,axis-0.09,len);
+    float b = smoothstep(axis+0.1,0.09,len);
     return a-b;
 }
 void main(){
     vec3 baseTexture = texture(material,textureCoord).rgb;
     float mask = texture(materialMask,textureCoord).g;
-    float tX = sqrt(baseTexture.r*sin(time));
-    float tY = sqrt(baseTexture.g* cos(time));
+    float tX = baseTexture.r;
+    float tY = baseTexture.g;
     float len = axisSmooth(tX,length(vec2(tX,tY)));
-    fragColor = vec4(len * baseTexture,1.0);
+    fragColor = vec4( baseTexture,1.0);
 }
 
