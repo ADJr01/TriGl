10b03255b919d11c0569ab46a6eecf157cb12bc3
Rahat Adnan <sthep90@gmail.com>
2025-10-07 22:33:06 +0600

    making a circle

--- src/main.cpp
+++ src/main.cpp
@@ -89,8 +89,8 @@ int main() {
         glUniform1f(iTime,static_cast<float>(time));
         //? handling Rest of the Drawing functions
         glClear(GL_COLOR_BUFFER_BIT);
-        glDrawArrays( GL_TRIANGLE_FAN,0,VERTEX_TO_DRAW_COUNT);
-        glClearColor(0.0, 0.0, 0.0, 0.0);
+        glDrawArrays( GL_TRIANGLES,0,VERTEX_TO_DRAW_COUNT);
+        glClearColor(1.0, 1.0, 1.0, 1.0);
         glBindVertexArray(0);
         glUseProgram(0);
     });
